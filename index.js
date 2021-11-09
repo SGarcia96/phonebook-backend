bf0d452aab1de83b9f743a251e05ac5db68e556c
sgarcia <sgcerverdadev@gmail.com>
2021-11-09 18:36:04 +0100

    add dockerfile

--- index.js
+++ index.js
@@ -88,5 +88,5 @@ app.use(handleErrors)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on http://localhost:${PORT}`)
 })
